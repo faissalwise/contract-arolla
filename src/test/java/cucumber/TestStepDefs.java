01fbe1b42be5af3d9646d3bae646bcc8df8e7c09
mrharnati <adm-azy-meryem.rharnati@chanel.com>
2023-01-09 17:00:14 +0100

    add first test and gitignore

--- src/test/java/cucumber/TestStepDefs.java
+++ src/test/java/cucumber/TestStepDefs.java
@@ -2,12 +2,21 @@ package cucumber;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class TestStepDefs {
 
-  @Then("fail")
-  void fail() {
-    assertThat(true).isFalse();
+  @Given("employee has a {string} contract with a base salary of {int}")
+  public void employeeHasAContractWithABaseSalaryOf(String contractType, int baseSalary) {
+  }
+
+  @When("employee has received his salary")
+  public void employeeHasReceivedHisSalary() {
+  }
+
+  @Then("the net salary is {int}")
+  public void theNetSalaryIs(int netSalary) {
   }
 }
--- src/test/resources/cucumber/test.feature
+++ src/test/resources/cucumber/test.feature
@@ -1,4 +1,6 @@
-Feature: Test
+Feature: Test Contracts
 
-  Scenario: First Test
-    Then fail
+  Scenario: Referring contract
+    Given employee has a "normal" contract with a base salary of 1000
+    When employee has received his salary
+    Then the net salary is 1000
