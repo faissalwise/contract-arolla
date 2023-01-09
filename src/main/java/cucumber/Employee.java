d0aa9411d7a8868aa0be5512bab67e1bac48ca6e
Antoine Alberti <aa@antoinealberti.com>
2023-01-09 17:47:53 +0100

    Introduce ContractType enum

--- src/main/java/cucumber/Employee.java
+++ src/main/java/cucumber/Employee.java
@@ -1,28 +1,30 @@
 package cucumber;
 
 public class Employee {
-
-  private final String contract;
+  private final ContractType contractType;
   private final int baseSalary;
   private int margin;
 
   public Employee(String contract, int baseSalary) {
-    this.contract = contract;
+    this.contractType = ContractType.valueOf(contract.toUpperCase());
     this.baseSalary = baseSalary;
   }
 
   public int getNetSalary() {
-    if ("referent".equals(contract)) {
-      return baseSalary + (margin - 35000) * 7 / 100;
-    }
-    if ("consulting".equals(contract)) {
-      return baseSalary + (margin - 35000) * 15 / 100;
-    }
-
-    return this.baseSalary;
+    return baseSalary + (margin - 35000) * contractType.percent / 100;
   }
 
   public void setMargin(int margin) {
     this.margin = margin;
+  }
+
+  private enum ContractType {
+    NORMAL(0), REFERENT(7), CONSULTING(15);
+
+    private final int percent;
+
+    ContractType(int percent) {
+      this.percent = percent;
+    }
   }
 }
